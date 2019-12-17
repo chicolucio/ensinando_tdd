2397ce508d18e805edd462c417d7683e8e810018
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2019-12-16 23:56:49 -0300

    Docstring funçao e_par

--- core/par.py
+++ core/par.py
@@ -1,4 +1,21 @@
 def e_par(inteiro):
+    """Função testa se inteiro é par.
+
+    Parameters
+    ----------
+    inteiro : int
+        Os conceitos de par e ímpar só fazem sentido para inteiros.
+
+    Returns
+    -------
+    bool
+        Retorna True ou False
+
+    Raises
+    ------
+    TypeError
+        Retorna TypeError caso não seja inteiro.
+    """
     if isinstance(inteiro, int):
         return inteiro % 2 == 0
     else:
